4592b36835915ca2da797658ea7899231357aa57
Dyst <50362174+Dyastinn@users.noreply.github.com>
2024-01-25 23:26:49 +0800

    Add project files.

---
+++ MuseVNWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MuseVNWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
