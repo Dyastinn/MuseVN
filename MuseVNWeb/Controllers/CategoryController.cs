f2cb0954136c47700a05c485601acf98a11dcac7
Dyst <50362174+Dyastinn@users.noreply.github.com>
2024-01-30 04:18:47 +0800

    added edit and delete

--- MuseVNWeb/Controllers/CategoryController.cs
+++ MuseVNWeb/Controllers/CategoryController.cs
@@ -16,5 +16,66 @@ namespace MuseVNWeb.Controllers{
         public IActionResult Create() {
             return View();
         }
+        [HttpPost]
+        public IActionResult Create(Category obj) { 
+            if(ModelState.IsValid){
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "A new category has been added";
+                return RedirectToAction("Index", "Category");
+            }
+            return View();
+            
+        }
+        public IActionResult Edit(int? id){
+            if(id == null || id == 0) {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Categories.Find(id);
+            if(categoryFromDb == null) {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "The category has been updated";
+                return RedirectToAction("Index", "Category");
+            }
+            return View();
+
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Categories.Find(id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Category? obj = _db.Categories.Find(id);
+            if(obj == null) return NotFound();
+            
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            TempData["error"] = "The category has been deleted";
+            return RedirectToAction("Index", "Category");
+ 
+
+        }
     }
 }
